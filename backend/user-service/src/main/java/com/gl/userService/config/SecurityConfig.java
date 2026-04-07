e198d6f596e441d09ac536111a115e5441500a95
Md Amanullah <md.amanullah@globallogic.com>
2026-04-07 15:24:41 +0530

    refactor:updated AuthenticationFilter

--- backend/user-service/src/main/java/com/gl/userService/config/SecurityConfig.java
+++ backend/user-service/src/main/java/com/gl/userService/config/SecurityConfig.java
@@ -2,10 +2,10 @@ package com.gl.userService.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -15,13 +15,13 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/register", "/api/users/login").permitAll()
-                        .anyRequest().authenticated()
-                );
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/api/users/register", "/api/users/login").permitAll()
+//                        .anyRequest().authenticated()
+//                );
+//        return http.build();
+//    }
 }
